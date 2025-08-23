600dec54b179aa2705cfd48125c7da93a46c52ce
lalit-tambe <lalittambe963@gmail.com>
2025-08-23 14:22:29 +0530

    Addition: Added 'select' to select only desired fields with $project

--- src/builder.js
+++ src/builder.js
@@ -3,7 +3,9 @@ export class MongogateBuilder {
     this._pipeline = [];
   }
 
-  // Adds conditions to the pipeline for $match
+  // ---------- WHERE ----------
+  // Adds conditions to the pipeline for `$match`
+  // .where({ a:1, b:2 }) | .where("field", value) | .where("field","op",value)
   where(fieldOrObj, opOrVal, maybeVal) {
     let cond = {};
 
@@ -29,6 +31,17 @@ export class MongogateBuilder {
     }
 
     this._pipeline.push({ $match: cond });
+    return this;
+  }
+
+  // ---------- SELECT ----------
+  // Add only selected fields in the output,works for `$project`
+  // .select(["email","role.name","subscriptions.plan.title"])
+  select(fields = []) {
+    if (!Array.isArray(fields))
+      throw new Error("select() expects an array of field paths");
+    this._project = this._project || {};
+    for (const f of fields) this._project[f] = 1;
     return this;
   }
 }
